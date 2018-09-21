5e8819070bb760c58802e8e374b6bf89d9b7e4e5
Michael Durrant <mmdurrant@gmail.com>
2018-09-21 09:31:50 +0000

    Updated script

--- homedir/mdenv.sh
+++ homedir/mdenv.sh
@@ -4,7 +4,7 @@ MYENV_DIR=~/.$MYENV
 
 [[ -d $MYENV_DIR ]] && rm -rf $MYENV_DIR
 git clone https://github.com/mmdurrant/env $MYENV_DIR
-[[ -s $MYENV_DIR/homedir/env.sh ]] && source $MYENV_DIR/homedir/env.sh
-set -x
+source $MYENV_DIR/homedir/env.sh
+
 update_all
-set +x
+
