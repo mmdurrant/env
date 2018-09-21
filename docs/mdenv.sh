6738c26ba6a887c7d1d1f64614af84f994b97fc7
Michael Durrant <mmdurrant@gmail.com>
2018-09-21 16:48:22 +0000

    Moved script back so symlink compiles correctly

--- docs/mdenv.sh
+++ docs/mdenv.sh
@@ -1,1 +1,10 @@
-../homedir/mdenv.sh
+#!/bin/bash
+MYENV=mdenv
+MYENV_DIR=~/.$MYENV
+
+[[ -d $MYENV_DIR ]] && rm -rf $MYENV_DIR
+git clone https://github.com/mmdurrant/env $MYENV_DIR
+source $MYENV_DIR/homedir/env.sh
+
+update_all
+
