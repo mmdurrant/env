259eb844d8e4acca616bf00a84e238d678a5db5e
Michael Durrant <mmdurrant@gmail.com>
2018-09-21 08:55:43 +0000

    Moved file to homedir where it makes more sense, symlink in docs

--- docs/mdenv.sh
+++ docs/mdenv.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-MYENV=mdenv
-MYENV_DIR=~/.$MYENV
-
-[[ -d $MYENV_DIR ]] && rm -rf $MYENV_DIR
-git clone https://github.com/mmdurrant/env $MYENV_DIR
-[[ -s $MYENV_DIR/homedir/env.sh ]] && (source $MYENV_DIR/homedir/env.sh || exit 1)
-
-update_all
-
-rm -rf $MYENV_DIR
+../homedir/mdenv.sh
